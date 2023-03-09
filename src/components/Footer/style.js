a4910eb62b19a4cb24548d5e5d7b2a59c63a75e0
DutraGames <gabrieldutraamarante@gmail.com>
2023-03-09 11:25:29 -0300

    Remove menu footer

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -9,29 +9,7 @@ flex-direction: column;
 padding: 4rem;
 `
 
-export const Links = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-gap: 1rem;
-padding-top: 1rem;
 
-`
-
-export const ButtonsLink = styled.a`
-color: #FFF;
-font-size: 2rem;
-cursor: pointer;
-transition: all .4s;
-
-&:hover{
-    opacity: .5;
-}
-
-@media (max-width: 600px) {
-    font-size: 1.4rem;
-}
-`
 
 export const Copy = styled.span`
 padding-top: 2rem;
