e6585c468a526e2b1e5428c5ea7b4b5ab2a9567b
DutraGames <gabrieldutraamarante@gmail.com>
2023-03-08 21:00:04 -0300

    add icon and title dynamic

--- src/pages/api/og.js
+++ src/pages/api/og.js
@@ -1,31 +1,76 @@
 import { ImageResponse } from '@vercel/og';
 
 export const config = {
-    runtime: 'edge',
+  runtime: 'edge',
 };
 
-export default async function handler() {
+export default function handler(req) {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    // ?title=<title>
+    const hasTitle = searchParams.has('title');
+    const title = hasTitle
+      ? searchParams.get('title')?.slice(0, 100)
+      : 'MONTANDO QUIZ';
+
     return new ImageResponse(
-        (
-            <div
-                style={{
-                    display: 'flex',
-                    fontSize: 72,
-                    color: 'white',
-                    background: 'black',
-                    width: '100%',
-                    height: '100%',
-                    textAlign: 'center',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}
-            >
-                <h1>MONTANDO QUIZ</h1>
-            </div>
-        ),
-        {
-            width: 1200,
-            height: 1200,
-        },
+      (
+        <div
+          style={{
+            backgroundColor: 'black',
+            backgroundSize: '150px 150px',
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            textAlign: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              justifyItems: 'center',
+            }}
+          >
+            <img
+              alt="Logo_Montando_Quiz"
+              height={300}
+              src="https://user-images.githubusercontent.com/69097449/223877961-30c4f8e7-fae9-40ff-a793-8ccfe5422569.png"
+              style={{ margin: '0 30px' }}
+              width={300}
+            />
+          </div>
+          <div
+            style={{
+              fontSize: 72,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              color: 'white',
+              marginTop: 30,
+              padding: '0 120px',
+              lineHeight: 1.4,
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            {title}
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      },
     );
+  } catch (e) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
 }
