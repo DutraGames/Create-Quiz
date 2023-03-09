5afa1cd61c36b368f77b94246de95afd005d301e
DutraGames <gabrieldutraamarante@gmail.com>
2023-03-09 11:25:43 -0300

    Add Menu in Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,39 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
-import { Container, Rotate, Title } from './style'
+import { ButtonsLink, Container, HeaderBottom, HeaderTop, Links, Rotate, Title } from './style'
 export default function Header() {
   return (
     <Container>
-      <Rotate>
-        <Image src='/logo.svg' width={40} height={40} alt="Logo Create Quiz"/>
-      </Rotate>
-      <Title>MONTANDO QUIZ</Title>
+      <HeaderTop>
+        <Rotate>
+          <Image src='/logo.svg' width={40} height={40} alt="Logo Create Quiz" />
+        </Rotate>
+        <Title>MONTANDO QUIZ</Title>
+      </HeaderTop>
+      <Links>
+        <Link href='/' legacyBehavior>
+          <ButtonsLink>Criar</ButtonsLink>
+        </Link>
+        <Link href='/perguntas?categoria=Portugues' legacyBehavior>
+          <ButtonsLink>Português</ButtonsLink>
+        </Link>
+        <Link href='/perguntas?categoria=Geografia' legacyBehavior>
+          <ButtonsLink>Geografia</ButtonsLink>
+        </Link>
+        <Link href='/perguntas?categoria=Matematica' legacyBehavior>
+          <ButtonsLink>Matemática</ButtonsLink>
+        </Link>
+        <Link href='/perguntas?categoria=Historia' legacyBehavior>
+          <ButtonsLink>História</ButtonsLink>
+        </Link>
+        <Link href='/perguntas?categoria=Ciencias' legacyBehavior>
+          <ButtonsLink>Ciências</ButtonsLink>
+        </Link>
+      </Links>
+      <HeaderBottom>
+
+      </HeaderBottom>
     </Container>
   )
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -14,12 +14,12 @@ export const Rotate = styled.div`
 `;
 
 export const Container = styled.div`
-padding: 0 11.2rem;
+padding: 2rem;
 background-color: #2E2E2E;
 display: flex;
-height: 5rem;
 align-items: center;
 justify-content: center;
+flex-direction: column;
 `
 
 export const Title = styled.h1`
@@ -31,5 +31,46 @@ padding-left: 5rem;
 @media (max-width: 600px) {
     font-size: 2rem;
     text-align: center;
+}
+`
+
+export const HeaderTop = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
+export const HeaderBottom = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
+export const Links = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+gap: 1rem;
+padding-top: 1rem;
+border-top: .1rem solid #3E3E3E;
+width: 100vw;
+`
+
+export const ButtonsLink = styled.a`
+color: #FFF;
+font-size: 2rem;
+cursor: pointer;
+transition: all .4s;
+padding: 1rem;
+
+&:hover{
+    opacity: .5;
+    background-color: #1E1E1E;
+}
+
+@media (max-width: 600px) {
+    font-size: 1.4rem;
+    padding: 0;
+
 }
 `
