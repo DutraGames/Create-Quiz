124494b9060eac4a6b4257ce5b764c692ef7de79
DutraGames <gabrieldutraamarante@gmail.com>
2023-03-09 23:04:38 -0300

    add getAuth in aplication

--- src/service/configFirebase.js
+++ src/service/configFirebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const database = getDatabase(app)
+export const auth = getAuth(app)
