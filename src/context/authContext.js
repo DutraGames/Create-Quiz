6e91123d2cbb2cc58f82dd7d1c8fcafea370cdaa
DutraGames <gabrieldutraamarante@gmail.com>
2023-03-10 13:18:53 -0300

    add new props

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -4,8 +4,9 @@ export const AuthContext = createContext()
 
 export default function AuthContextProvider({children}){
     const [Logado, setLogado] = useState(false)
+    const [nome, setNome] = useState("")
     return(
-        <AuthContext.Provider value={{Logado, setLogado}}>
+        <AuthContext.Provider value={{Logado, setLogado,nome, setNome}}>
             {children}
         </AuthContext.Provider>
     )
