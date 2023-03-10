b8cd8264a60da0c16b35594f431f439a0350e30a
DutraGames <gabrieldutraamarante@gmail.com>
2023-03-09 23:08:05 -0300

    add verication and cmponent logado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,29 @@
+import { onAuthStateChanged } from 'firebase/auth'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
-import { ButtonsLink, Container, HeaderBottom, HeaderTop, Links, Rotate, Title } from './style'
+import React, { useEffect } from 'react'
+import { AiOutlineUser } from 'react-icons/ai'
+import useAuth from '../../hooks/useAuth'
+import { auth } from '../../service/configFirebase'
+import { BtnLogado, ButtonsLink, Container, HeaderBottom, HeaderTop, Links, Rotate, Title } from './style'
 export default function Header() {
+
+  const {Logado, setLogado} = useAuth()
+  
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        setLogado(true)
+
+      }else{
+
+      }
+    });
+  }, [])
+  
+  
   return (
     <Container>
       <HeaderTop>
@@ -10,6 +31,7 @@ export default function Header() {
           <Image src='/logo.svg' width={40} height={40} alt="Logo Create Quiz" />
         </Rotate>
         <Title>MONTANDO QUIZ</Title>
+        {Logado? (<BtnLogado><AiOutlineUser size="2rem"/></BtnLogado>):(<div></div>)}
       </HeaderTop>
       <Links>
         <Link href='/' legacyBehavior>
