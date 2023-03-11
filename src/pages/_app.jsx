3688e218a02003ad39d6f781ef76967e80a32b72
DutraGames <gabrieldutraamarante@gmail.com>
2023-03-11 20:39:30 -0300

    add toastContainer and config

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,22 @@
-import AuthContextProvider from '../context/authContext'
-import GlobalStyle from '../styles/globalstyle'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AuthContextProvider from '../context/authContext';
+import GlobalStyle from '../styles/globalstyle';
 export default function MyApp({ Component, pageProps }) {
     return (
         <AuthContextProvider>
             <GlobalStyle />
             <Component {...pageProps} />
+            <ToastContainer position="bottom-left"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored" />
         </AuthContextProvider>
     )
 }
