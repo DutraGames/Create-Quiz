6dc041d96f6391c49c6b3ba69ed04274761b309a
DutraGames <gabrieldutraamarante@gmail.com>
2023-03-11 16:48:10 -0300

    min height

--- src/styles/app.js
+++ src/styles/app.js
@@ -1,5 +1,9 @@
 import styled from "styled-components"
 
+export const Container = styled.div`
+min-height: 100vh;
+`
+
 export const FrameQuestion = styled.div`
 background-color: #FFE7A8;
 width: 50rem;
