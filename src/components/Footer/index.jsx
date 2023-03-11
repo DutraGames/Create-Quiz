1c6c9d5841f7ba8eb08d22691b7efec42b78d6b8
DutraGames <gabrieldutraamarante@gmail.com>
2023-03-11 19:24:17 -0300

    add links in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import { Container, Copy } from './style'
+import { AiFillGithub, AiFillInstagram } from 'react-icons/ai'
+import { ButtomLink, Container, Copy } from './style'
+
 export default function Footer() {
   return (
     <Container>
+      <ButtomLink href='https://github.com/DutraGames/Create-Quiz'>
+        <AiFillGithub color='#FFF' size='3rem' />
+      </ButtomLink>
       <Copy>&copy; Todos os direitos reservados</Copy>
+      <ButtomLink href='https://www.instagram.com/dutrinha_dev' >
+        <AiFillInstagram color='#FFF' size='3rem' />
+      </ButtomLink>
     </Container>
   )
 }
