2911573e7841ea1a2c1450d11802420189042c76
DutraGames <gabrieldutraamarante@gmail.com>
2023-04-01 10:24:58 -0300

    add Amount questions array

--- src/pages/perguntas.jsx
+++ src/pages/perguntas.jsx
@@ -2,15 +2,15 @@ import { onValue, ref, remove } from 'firebase/database'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import Footer from '../components/Footer'
-import Header from '../components/Header'
 import HeadMain from '../components/HeadMain'
+import Header from '../components/Header'
 import Question from '../components/Question'
 import { database } from '../service/configFirebase'
-import { Container } from '../styles/question'
+import { Container, FieldAmount } from '../styles/question'
 export default function Perguntas() {
 
     const [Perguntas, setPerguntas] = useState([])
-    
+
     const router = useRouter()
     const categoria = router.query.categoria
 
@@ -47,13 +47,14 @@ export default function Perguntas() {
             <HeadMain title={`Questões de ${categoria} `} desc={`Questões de ${categoria} descritas por professores!`} />
             <Header />
             <Container>
+                <FieldAmount> Há {Perguntas.length} perguntas</FieldAmount>
                 {
-                    Perguntas.length > 0?                     Perguntas.map((pergunta) => {
+                    Perguntas.length > 0 ? Perguntas.map((pergunta) => {
 
                         return (
-                            <Question data={pergunta} DeleteQuestion={DeleteQuestion} categoria={categoria}/>
+                            <Question data={pergunta} DeleteQuestion={DeleteQuestion} categoria={categoria} />
                         )
-                    }): (<h1>Não há questões registradas!</h1>)
+                    }) : (<></>)
                 }
             </Container>
 
--- src/styles/question.js
+++ src/styles/question.js
@@ -5,3 +5,7 @@ padding: 2rem;
 min-height: 100vh;
 `
 
+export const FieldAmount = styled.p`
+text-align: center;
+font-size: 2rem;]
+`
